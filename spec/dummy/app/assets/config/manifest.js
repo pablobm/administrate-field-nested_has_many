3d859a4ab8ee91b2af89a291f1598d98471afebc
Pablo Brasero <pablo@pablobm.com>
2020-03-14 17:46:04 +0000

    To work with Sprockets 4

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link administrate/application.css
+//= link administrate/application.js
+//= link administrate-field-nested_has_many/application.css
+//= link administrate-field-nested_has_many/application.js
